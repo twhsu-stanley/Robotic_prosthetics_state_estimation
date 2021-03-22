1b2abab9dae0a8acbe0776365974dc4ea35a358c
twhsu <twhsu@umich.edu>
2021-03-22 13:58:47 -0400

    PF heatmap

--- PF.py
+++ PF.py
@@ -3,6 +3,7 @@ from numpy import matlib
 from numpy.random import randn, rand
 from scipy.stats import multivariate_normal
 from model_framework import *
+import math
 
 def warpToOne(phase):
     phase_wrap = np.remainder(phase, 1)
@@ -33,8 +34,9 @@ class particle_filter:
         self.LQ = np.linalg.cholesky(self.Q)  # Cholesky factor of Q
         self.h = system.h  # measurement model
         self.R = system.R  # measurement noise covariance
-        self.n = init.n  # number of particles
-        self.pp = 0.8
+        self.n = init.n  # dynamic number of particles
+        self.pn = init.n # number of particles
+        self.delta = 0
 
         self.p = myStruct()  # particles
         self.mu = init.mu # particles mean
@@ -50,79 +52,82 @@ class particle_filter:
 
     def particles_propagation(self, dt):
         for i in range(self.n):
-            # process noise
-            pn = np.dot(self.LQ, randn(4, 1))
-            # propagate the particles
+            pn = np.dot(self.LQ, randn(4, 1)) # process noise
             self.p.x[i, :] = self.f(np.array([self.p.x[i, :]]).T, dt, pn).reshape(-1)
-            #self.p.x[i,0] = warpToOne(self.p.x[i,0])
         
     def importance_measurement(self, z, Psi):
         # Inputs:
         #   z: measurement
         z = np.array([z]).T
         w = np.zeros((self.n, 1))  # importance weights
+        count = 0
         for i in range(self.n):
             z_hat = self.h.evaluate_h_func(Psi, warpToOne(self.p.x[i,0]), self.p.x[i,1], self.p.x[i,2], self.p.x[i,3])
-            v = (z - z_hat).reshape(-1)
-            w[i] = multivariate_normal.pdf(v, np.array([0, 0, 0, 0]), self.R)
+            w[i] = multivariate_normal.pdf((z - z_hat).reshape(-1), np.array([0, 0, 0, 0]), self.R)
+            if w[i] > 1e-85:
+                count += 1
 
-        # update and normalize weights
-        self.p.w = np.multiply(self.p.w, w)
-
-        if np.sum(self.p.w) < 1e-80: # might be kidnapped
+        if count < self.n / 10: # might be kidnapped
             self.Neff = 0
-            self.resampling()
-            # compute mean and covariance of estimate
-            self.mean_cov()
+            self.n = self.pn * 10 # increase number of particles by 10 times
+            self.resampling(mode = "uniform")
         else:
+            # update and normalize weights
+            self.p.w = np.multiply(self.p.w, w)
             self.p.w = self.p.w / np.sum(self.p.w)
-            # compute effective number of particles
             self.Neff = 1 / np.sum(np.power(self.p.w, 2))  # effective number of particles
-            if self.Neff < self.n * 0.2:
-                self.resampling()
-            # compute mean and covariance of estimate
-            self.mean_cov()
-
-    def resampling(self):
-        # low variance resampling
-        if np.sum(self.p.w) < 1e-80:
-            M = 0
-            print("resamping: generate uniformly distributed state")
-        else:
-            M = round(self.n * 1)
-
-        W = np.cumsum(self.p.w)
-        r = rand(1) / self.n
-        j = 1
-        for m in range(M):
-            u = r + (m - 1) / self.n
-            while u > W[j]:
-                j = j + 1
-            self.p.x[m, :] = self.p.x[j, :]
-            self.p.w[m] = 1 / self.n
+            if self.Neff < self.n / 5:
+                self.n = self.pn # set number of pparticles to the origin value
+                self.resampling(mode = "low_variance")
         
-        for m in range(M, self.n):
-            phase_kidnap = np.random.uniform(-0.5, 0.5)
-            phase_dot_kidnap = np.random.uniform(0, 5)
-            step_length_kidnap = np.random.uniform(0, 2)
-            ramp_kidnap = np.random.uniform(-45, 45)
-            self.p.x[m, :] = [self.p.x[m,0] + phase_kidnap, phase_dot_kidnap, step_length_kidnap, ramp_kidnap]
-            self.p.w[m] = 1 / self.n
+        # compute mean and covariance of estimate
+        self.mean_cov()
+
+    def resampling(self, mode):
+        if mode == "uniform":
+            # generate uniformly distributed state
+            #print("resamping: uniform")
+            mu_temp = np.mean(self.p.x[:, 0])
+            self.p.x = []
+            self.p.w = []
+            for m in range(self.n):
+                phase_kidnap = np.random.uniform(0, 1)
+                phase_dot_kidnap = np.random.uniform(0, 5)
+                step_length_kidnap = np.random.uniform(0, 2)
+                ramp_kidnap = np.random.uniform(-45, 45)
+
+                self.p.x.append([math.floor(mu_temp) + phase_kidnap, phase_dot_kidnap, step_length_kidnap, ramp_kidnap])
+                self.p.w.append(1 / self.n)
+            self.p.x = np.array(self.p.x).reshape(-1, 4)
+            self.p.w = np.array(self.p.w).reshape(-1, 1)
+
+        elif mode == "low_variance":
+            # low variance resampling
+            #print("resamping: low_variance")
+            W = np.cumsum(self.p.w)
+            r = rand(1) / self.n
+            j = 0
+            x_temp = self.p.x
+            self.p.x = []
+            self.p.w = []
+            for m in range(self.n):
+                u = r + m / self.n
+                while u > W[j]:
+                    j = j + 1
+                
+                self.p.x.append(x_temp[j, :]) #self.p.x[m, :] = self.p.x[j, :]
+                self.p.w.append(1 / self.n) #self.p.w[m] = 1 / self.n
+            self.p.x = np.array(self.p.x).reshape(-1, 4)
+            self.p.w = np.array(self.p.w).reshape(-1, 1)
 
     def mean_cov(self):
-        M = round(self.n * 1) #self.pp
-        wtot = np.sum(self.p.w[0:M])
+        wtot = np.sum(self.p.w)
         if wtot > 0:
-            #self.mu[0] = np.sum(self.p.x[:, 0] * self.p.w.reshape(-1)) / wtot
-            #self.mu[1] = np.sum(self.p.x[:, 1] * self.p.w.reshape(-1)) / wtot
-            #self.mu[2] = np.sum(self.p.x[:, 2] * self.p.w.reshape(-1)) / wtot
-            #self.mu[3] = np.sum(self.p.x[:, 3] * self.p.w.reshape(-1)) / wtot
+            self.mu[0] = np.sum(self.p.x[:, 0] * self.p.w.reshape(-1)) / wtot
+            self.mu[1] = np.sum(self.p.x[:, 1] * self.p.w.reshape(-1)) / wtot
+            self.mu[2] = np.sum(self.p.x[:, 2] * self.p.w.reshape(-1)) / wtot
+            self.mu[3] = np.sum(self.p.x[:, 3] * self.p.w.reshape(-1)) / wtot
             
-            self.mu[0] = np.sum(self.p.x[0:M, 0] * self.p.w[0:M].reshape(-1)) / wtot
-            self.mu[1] = np.sum(self.p.x[0:M, 1] * self.p.w[0:M].reshape(-1)) / wtot
-            self.mu[2] = np.sum(self.p.x[0:M, 2] * self.p.w[0:M].reshape(-1)) / wtot
-            self.mu[3] = np.sum(self.p.x[0:M, 3] * self.p.w[0:M].reshape(-1)) / wtot
-
             #sum = 0
             #for i in range(self.n):
             #    dev = self.p.x[i, :].T - self.mu
@@ -138,19 +143,7 @@ class particle_filter:
             self.mu[2] = np.nan
             self.mu[3] = np.nan
 
-    def kidnap(self):
-        # since kidnapping always happens in the 1st stride, wrapToOne is not needed
-        phase_kidnap = np.random.uniform(-0.5, 0.5)
-        phase_dot_kidnap = np.random.uniform(0, 5)
-        step_length_kidnap = np.random.uniform(0, 2)
-        ramp_kidnap = np.random.uniform(-45, 45)
-        state_kidnap = np.array([[self.mu[0, 0] + phase_kidnap], [phase_dot_kidnap], [step_length_kidnap], [ramp_kidnap]])
-        
-        delta = state_kidnap - self.mu
-        self.p.x = self.p.x + matlib.repmat(delta.T, self.n, 1)
-        print("kidnap delta = \n", delta)
-
-
-
-
-
+    def kidnap(self, state_kidnap):
+        self.delta = state_kidnap - self.mu
+        self.p.x = self.p.x + matlib.repmat(self.delta.T, self.n, 1)
+        print("kidnap delta = \n", self.delta)
--- PF_robustness_test.py
+++ PF_robustness_test.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from PF import *
 from model_framework import *
@@ -28,12 +31,12 @@ def pf_test(subject, trial, side, kidnap = True, plot = True):
     sys = myStruct()
     sys.f = process_model
     sys.h = measurement_model
-    sys.Q = np.diag([1e-14, 1e-6, 1e-7, 5e-5]) # process model noise covariance
+    sys.Q = np.diag([1e-14, 1e-7, 1e-7, 5e-5]) # process model noise covariance
     sys.R = R[subject] # measurement noise covariance
 
     # initialization
     init = myStruct()
-    init.n = 200
+    init.n = 100
     init.mu = np.array([[phases[0]], [phase_dots[0]], [step_lengths[0]], [ramps[0]]])
     #init.Sigma = np.diag([1e-14, 5e-4, 1e-3, 1e-1])
     init.Sigma = np.diag([1e-14, 1e-14, 1e-14, 1e-14])
@@ -49,7 +52,14 @@ def pf_test(subject, trial, side, kidnap = True, plot = True):
     heel_strike_index = Conti_heel_strikes(subject, trial, side) - Conti_heel_strikes(subject, trial, side)[0]
     kidnap_index = np.random.randint(heel_strike_index[0], heel_strike_index[1]) # step at which kidnapping occurs
 
-    total_step = 300 # = np.shape(z)[1]
+    # kidnapping state
+    phase_kidnap = np.random.uniform(0, 1)
+    phase_dot_kidnap = np.random.uniform(0, 5)
+    step_length_kidnap = np.random.uniform(0, 2)
+    ramp_kidnap = np.random.uniform(-45, 45)
+    state_kidnap = np.array([[phase_kidnap], [phase_dot_kidnap], [step_length_kidnap], [ramp_kidnap]])
+
+    total_step = 800 # = np.shape(z)[1]
     phases = phases[0 : total_step]
     phase_dots = phase_dots[0 : total_step]
     step_lengths = step_lengths[0 : total_step]
@@ -61,7 +71,7 @@ def pf_test(subject, trial, side, kidnap = True, plot = True):
     for i in range(total_step):
         # kidnap
         if kidnap == True and i == kidnap_index:
-            pf.kidnap()
+            pf.kidnap(state_kidnap)
             print("kidnap index = ", kidnap_index)
         
         pf.particles_propagation(dt)
@@ -72,90 +82,96 @@ def pf_test(subject, trial, side, kidnap = True, plot = True):
         Neff[i] = pf.Neff
 
     # evaluate robustness
-    # compare x and ground truth:
-    #track = True
-    track_tol = 0.07
-    #start_check = 2
+    track = True
+    track_tol = 0.08
+    start_check = 4
     se = 0
     #for i in range(int(heel_strike_index[0]), int(heel_strike_index[np.size(heel_strike_index)-1])):
-    for i in range(kidnap_index[0], total_step):
+    for i in range(total_step):
         error_phase = phase_error(x[i, 0], phases[i])
         se += error_phase ** 2
-        #if i >= int(heel_strike_index[start_check]):
-            #track = track and (error_phase < track_tol)
+        if i >= int(heel_strike_index[start_check]):
+            track = track and (error_phase < track_tol)
     
-    RMSE_phase = np.sqrt(se / np.size(phases))
-    track = (RMSE_phase < track_tol)
+    RMSE_phase = np.sqrt(se / total_step)
+    #track = (RMSE_phase < track_tol)
     print("RMSE phase = ", RMSE_phase)
 
     if kidnap == True:
         print("recover from kidnap? ", track)
+        phase_dot_akn = x[kidnap_index, 1][0]
+        phase_dot_b4kn = x[kidnap_index - 1, 1][0]
+        print("phase_dot right after kidnap = ", phase_dot_akn)
+        print("phase_dot right before kidnap = ", phase_dot_b4kn)
     elif kidnap == False:
         print("track without kidnapping? ", track)
 
     # plot results
     if plot == True:
-        plt.figure(0)
+        plt.figure('Effective number of particles')
         plt.plot(Neff)
         plt.ylabel('Neff')
 
-        plt.figure(1)
+        plt.figure('Estimates')
         plt.subplot(411)
         plt.plot(phases)
         plt.plot(x[:, 0], '--')
         plt.ylabel('phase')
+
         plt.subplot(412)
         plt.plot(phase_dots)
         plt.plot(x[:, 1], '--')
         plt.ylabel('phase dot')
-        #plt.ylim(phase_dots.min()-0.2, phase_dots.max() +0.2)
+
         plt.subplot(413)
         plt.plot(step_lengths)
         plt.plot(x[:, 2], '--')
-        #plt.ylim(step_lengths.min()-0.02, step_lengths.max() +0.02)
         plt.ylabel('step length')
+
         plt.subplot(414)
         plt.plot(ramps)
         plt.plot(x[:, 3], '--')
         plt.ylabel('ramp')
         plt.show()
     
-    return track, RMSE_phase
+    return track, RMSE_phase, phase_dot_b4kn, phase_dot_akn
 
 def pf_robustness(kidnap = True, RMSE_heatmap = False):    
     track_count = 0
     total_trials = 0
-    RMSerror_phase = np.zeros((10, 27))
-    s = 0
-    for subject in Conti_subject_names():
-    #for subject in ['AB05']:
+    RMSerror_phase = []
+
+    #for subject in Conti_subject_names():
+    for subject in ['AB01', 'AB10']:
         print("subject: ", subject)
-        t = 0
+
         for trial in Conti_trial_names(subject):
+        #for trial in ['s1x2d2x5']:
             if trial == 'subjectdetails':
                 continue
             print("trial: ", trial)
             for side in ['left']:
                 #print("side: ", side)
                 total_trials = total_trials + 1
-                track, RMSE_phase = pf_test(subject, trial, side, kidnap = True, plot = False)
+                track, RMSE_phase, phase_dot_b4kn, phase_dot_akn = pf_test(subject, trial, side, kidnap = True, plot = False)
+                RMSerror_phase.append([RMSE_phase, phase_dot_b4kn, phase_dot_akn])
                 if  track == True:
                     track_count = track_count + 1
-                RMSerror_phase[s, t] = RMSE_phase
-            t += 1
-        s += 1
-
-    with open('RMSE_phase_PF.pickle', 'wb') as file:
-        pickle.dump(RMSerror_phase, file)
-
-    if RMSE_heatmap:
-        plt.imshow(RMSerror_phase)
-        plt.title("RMSE of phase")
-        plt.xlabel("trials")
-        plt.ylabel("subjects")
-        plt.colorbar()
-        plt.show()
+    
     robustness = track_count / total_trials * 100
+
+    RMSerror_phase = np.array(RMSerror_phase).reshape(-1, 3)
+    with open('RMSE_phase_PF.pickle', 'wb') as file:
+            pickle.dump(RMSerror_phase, file)
+    
+    if RMSE_heatmap == True:
+        RMSerror_phase_df = pd.DataFrame(RMSerror_phase, columns=['RMSE', 'x', 'y'])
+        sns.heatmap(RMSerror_phase_df.pivot('y', 'x', 'RMSE'))
+        plt.title("RMSE of phase")
+        plt.xlabel("phase_dot before kidnapping")
+        plt.ylabel("phase_dot after kidnapping")
+        plt.show()
+
     return robustness
 
 if __name__ == '__main__':
@@ -163,13 +179,10 @@ if __name__ == '__main__':
     trial= 's1x2d2x5'
     side = 'left'
 
-    pf_test(subject, trial, side, kidnap = True, plot = True)
+    #pf_test(subject, trial, side, kidnap = True, plot = True)
+    pf_robustness(kidnap = True, RMSE_heatmap = True)
 
     #Q = np.diag([1e-14, 1e-7, 1e-7, 5e-5]) # process model noise covariance
     #print("Q =\n", Q)
     #robustness = ekf_robustness(Q, kidnap = False, RMSE_heatmap = True)
     #print("robustness (%): ", robustness)
-
-
-
-
